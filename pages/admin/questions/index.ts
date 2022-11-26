5db2b9723999cc197101f8fe23dad806968dd180
m11o <m11o@hey.jp>
2022-11-27 00:38:49 +0900

    add admin/questions/index.tsx

---
+++ pages/admin/questions/index.ts
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Card } from "@chakra-ui/card";
+
+import Admin from '../../../src/layout/admin'
+
+type QuestionsProps = {
+}
+const Questions: React.FC<QuestionsProps> = () => {
+  return (
+    <Admin>
+      <p>hoge</p>
+    </Admin>
+  )
+}
+export default Questions
